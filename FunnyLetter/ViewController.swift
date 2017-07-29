086ebdc7ffdbe7bff242781527756f2b6466748a
Smaine Milianni <smainemilianni@MacBook-Pro-de-Smaine.local>
2017-07-29 09:15:44 +0200

    Initial Commit

---
+++ FunnyLetter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunnyLetter
+//
+//  Created by Smaine Milianni on 29/07/2017.
+//  Copyright © 2017 Smaine Milianni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
